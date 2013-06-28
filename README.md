cd7bc9d532037bc2ebd138ff7cfd4dec0ccd2fc8
Anthony Green <green@moxielogic.com>
2013-06-28 15:03:34 -0400

    First push

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 autofile
 ========
 
-Content based PDF filer for your paperless office.
+Autofile is a simple script written in Common Lisp to help sort PDF
+files based on their content.
+
+My "paperless-office" workflow looks something like this...
+
+1. Scan all mail and receipts with OCR-powered tools to create
+   searchable PDF files.
+
+2. Run autofile to extract text from PDF documents and generate a
+   series of shell commands to rename the files and sort them into
+   appropriate directories.
+
+3. Dump paper in long term storage.
+
+Autofile runs on simple sorting rules.  I have enough rules on my
+system that it catches virtually all recurring mail and expense
+receipts.  Autofile can also extract relevant date information from
+the documents which is used in the new filed document name.
+
+Modify *scanner-file* in autofile.lisp to point to your filing rules.
+
+    (defscanner *scanners*
+
+      ;; What follows are a series of 3 element lists.
+    
+      ;; The three elements are as follows:
+      ;; 1. The format string for the new file name.
+      ;; 2. A list of regexps that must match in the PDF.
+      ;; 3. A regexp to extract the data from the document.  This	
+      ;;    date is used in generating the new filename.
+    
+      ;; Here are some examples:
+    
+      ;; Taxi Expense Receipts
+      ("~A/Work/Expenses/NEW/~4,'0d~2,'0d~2,'0d-taxi.pdf"
+       ("TAXI" "CUSTOMER COPY")
+       "([0-9][0-9]/[0-9][0-9]/[0-9][0-9])")
+    
+       ;; Great-West Life Explanation of Benefits
+       ("~A/Financial/Insurance/GreatWest/Payments/~4,'0d~2,'0d~2,'0d-gw-payment.pdf"
+        ("Great-West" "WE HAVE REVIEWED YOUR CLAIM")
+        "EXPLANATION\ *OF\ *BENEFITS[\ ]\+([^\ ]\+[\ ]\+[0-9]\+,[\ ]\+20[0-9][0-9])")
+    
+       ;; 407ETR
+       ("~A/Personal/Receipts/407ETR/~4,'0d~2,'0d~2,'0d-407ETR-stmt.pdf"
+        ("407 ETR" "Account summary")
+        "[\ ]\+([0-9]\+[\ ]\*[A-Ya-y]\+[\ ]\+[0-9][0-9])")
+     
+       ;; Sheraton Hotels Expense Receipts
+       ("~A/Work/Expenses/NEW/~4,'0d~2,'0d~2,'0d-hotel.pdf"
+        ("Sheraton" "HOTELS & RESORTS")
+        "Depart Date[\ ]\+([^\ ]\+-\+[A-Y]\+-[0-9][0-9])")
+    )
+
+Sorry for the sparse docs, and good luck!
+
+Anthony Green
+green@moxielogic.com
---
+++ autofile.lisp
@@ -0,0 +1,110 @@
+;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: autofile; Base: 10 -*-
+
+;;; Copyright (C) 2013  Anthony Green <green@moxielogic.com>
+
+;;; autofile is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published
+;;; by the Free Software Foundation; either version 3, or (at your
+;;; option) any later version.
+;;;
+;;; autofile is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with autofile; see the file COPYING3.  If not see
+;;; <http://www.gnu.org/licenses/>.
+
+;;;
+;;; Sorts PDF documents based on content
+;;;
+
+(ql:quickload :cl-ppcre)
+(ql:quickload :cl-fad)
+(ql:quickload :trivial-shell)
+(ql:quickload :net-telent-date)
+
+(defpackage :autofile
+  (:use :cl :cl-user :cl-ppcre :cl-fad))
+
+(in-package :autofile)
+
+(defparameter *capture-dir* "/home/green/Dropbox/ScanSnap"
+  "The directory from which to read PDF files.")
+
+(defparameter *target-dir* "/home/green/TOL"
+  "The top level directory of our document filing hierarchy.")
+
+(defparameter *review-dir* "/home/green/TOL/REVIEW"
+  "The directory where we'll drop files that we know need to be reviewed")
+
+(defparameter *scanner-file* "/home/green/TOL/bin/scanners.lisp"
+  "This file contains the scanner definitions.")
+
+(defmacro defscanner (name &rest scanners)
+  "This macro defines scanners used by the document sorter."
+  (list 'defparameter name 
+	(cons 'list
+	      (mapcar (lambda (s)
+			(list 'list
+			      (car s)
+			      (cons 'list
+				    (mapcar (lambda (r)
+					      `(create-scanner ,r :single-line-mode t))
+					    (car (cdr s))))
+			      `(create-scanner ,(car (cdr (cdr s))) :single-line-mode t)))
+		      scanners))))
+
+;; Load the scanner definitions.
+(load *scanner-file*)
+
+(defun test-rulez (scan-rule contents)
+  (let ((new-filename-format (car scan-rule))
+	(scan-list (car (cdr scan-rule)))
+	(date-match (car (cdr (cdr scan-rule))))
+	(fail nil))
+    (mapcar
+     (lambda (scanner) 
+       (if (not (scan scanner contents))
+	   (setf fail t)))
+     scan-list)
+    (if (not fail)
+	(progn
+	  (if date-match
+	      (multiple-value-bind (match date)
+		  (scan-to-strings date-match contents)
+		(declare (ignore match))
+		(if (not (eq nil date))
+		    (let ((utc (net.telent.date:parse-time (aref date 0))))
+		      (net.telent.date:with-decoding 
+		       (utc)
+		       (format nil new-filename-format
+			       *target-dir*
+			       net.telent.date:year
+			       net.telent.date:month
+			       net.telent.date:day-of-month)))))
+	    new-filename-format))
+      (not fail))))
+
+(let ((files (list-directory *capture-dir*)))
+  (mapcar (lambda (filename)
+	    (if (string= (pathname-type (pathname filename)) "pdf")
+		(multiple-value-bind (output error-output exit-status)
+		    (trivial-shell:shell-command (concatenate 'string "pdftotext -layout "
+							      (namestring filename)
+							      " -"))
+		  (declare (ignore error-output))
+		  (if (eq exit-status 0)
+		      (progn
+			(mapcar (lambda (rule)
+				  (let ((new-filename (test-rulez rule output)))
+				    (if new-filename
+					(format t "mv ~A ~A~%" filename new-filename))))
+				*scanners*))))))
+	  files))
+		  
+(sb-ext:quit)
+
+
+
